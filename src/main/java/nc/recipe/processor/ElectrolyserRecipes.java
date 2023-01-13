588a1a29534f1fe01dd22dd94f9e913d944e3c6f
mossy84 <71836363+mossy84@users.noreply.github.com>
2023-01-13 02:22:37 -0500

    Update ElectrolyserRecipes.java

--- src/main/java/nc/recipe/processor/ElectrolyserRecipes.java
+++ src/main/java/nc/recipe/processor/ElectrolyserRecipes.java
@@ -32,6 +32,8 @@ public class ElectrolyserRecipes extends ProcessorRecipeHandler {
 		addIsotopeFluorideRecipes("curium", 243, 245, 246, 247);
 		addIsotopeFluorideRecipes("berkelium", 247, 248);
 		addIsotopeFluorideRecipes("californium", 249, 250, 251, 252);
+		addIsotopeFluorideRecipes("einsteinium", 253, 254, 255);
+		addIsotopeFluorideRecipes("fermium", 254, 255, 257);
 		
 		addRecipe(fluidStack("fuel_tbu_fluoride", FluidStackHelper.INGOT_VOLUME), fluidStack("fuel_tbu", FluidStackHelper.INGOT_VOLUME), fluidStack("fluorine", FluidStackHelper.BUCKET_VOLUME), emptyFluidStack(), emptyFluidStack(), 0.5D, 1D);
 		addRecipe(fluidStack("depleted_fuel_tbu_fluoride", FluidStackHelper.INGOT_VOLUME), fluidStack("depleted_fuel_tbu", FluidStackHelper.INGOT_VOLUME), fluidStack("fluorine", FluidStackHelper.BUCKET_VOLUME), emptyFluidStack(), emptyFluidStack(), 0.5D, 1D);
@@ -42,6 +44,8 @@ public class ElectrolyserRecipes extends ProcessorRecipeHandler {
 		addFissionFuelFluorideRecipes("ecm", 243, 245, 247);
 		addFissionFuelFluorideRecipes("eb", 248);
 		addFissionFuelFluorideRecipes("ecf", 249, 251);
+		addFissionFuelFluorideRecipes("ees", 254);
+		addFissionFuelFluorideRecipes("efm", 255);
 	}
 	
 	public void addElementFluorideRecipes(String... elements) {
